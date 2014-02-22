e86ce98b9da0ead7012739f81b3d11b2711ea72f
augustocdias <augusto.c.dias@gmail.com>
2014-02-22 16:29:59 -0300

    adicionado leitura dos interceptors e plugins

--- src/main/scala/org/teaserver/Main.scala
+++ src/main/scala/org/teaserver/Main.scala
@@ -3,6 +3,8 @@ package org.teaserver
 import org.eclipse.jetty.server.Server
 import com.typesafe.config.{ConfigValue, ConfigFactory}
 import org.teaserver.interceptor.InterceptorFactory
+import java.util.ServiceLoader
+import org.teaserver.plugin.PluginFactory
 
 object Main {
   def main(args: Array[String]) {
@@ -11,9 +13,10 @@ object Main {
       val obj = Class.forName(i.unwrapped().toString).newInstance
       obj.asInstanceOf[InterceptorFactory].createInterceptor(config)
     }
+    val plugins = for (c: PluginFactory <- ServiceLoader load classOf[PluginFactory]) yield c.create(config)
 
     val server = new Server(8080);
-    server.setHandler(new RequestHandler(interceptors));
+    server.setHandler(new RequestHandler(interceptors, plugins));
     server.start;
     server.join;
   }
--- src/main/scala/org/teaserver/RequestHandler.scala
+++ src/main/scala/org/teaserver/RequestHandler.scala
@@ -4,8 +4,9 @@ import org.eclipse.jetty.server.handler.AbstractHandler
 import org.eclipse.jetty.server.Request
 import javax.servlet.http.{HttpServletResponse, HttpServletRequest}
 import org.teaserver.interceptor.TeaInterceptor
+import org.teaserver.plugin.TeaPlugin
 
-class RequestHandler (interceptors: List[TeaInterceptor]) extends AbstractHandler {
+class RequestHandler(interceptors: List[TeaInterceptor], plugins: List[TeaPlugin]) extends AbstractHandler {
   def handle(target: String, baseRequest: Request, request: HttpServletRequest, response: HttpServletResponse) {
     response.setContentType("text/html;charset=utf-8")
     response.setStatus(HttpServletResponse.SC_OK)
